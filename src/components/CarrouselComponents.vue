e70c1c76785028025f4f1be35523038484793b8f
RFENZL <92161747+RFENZL@users.noreply.github.com>
2024-11-19 09:19:04 +0100

    carrousel

---
+++ src/components/CarrouselComponents.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-carousel :cycle="cycle" :height="height" show-arrows>
+    <v-carousel-item
+      v-for="(image, index) in images"
+      :key="index"
+      :src="image"
+    >
+      <!-- Contenu supplémentaire -->
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+// Importation des images locales
+import Joconde from '@/assets/Tableau/Joconde.jpg';
+import LeCri from '@/assets/Tableau/Le Cri.png';
+import Monet from '@/assets/Tableau/Monet.png';
+import Picasso from '@/assets/Tableau/Picasso.png';
+import Tournessol from '@/assets/Tableau/Tournesols Van Gogh.jpg';
+
+export default {
+  name: "CarrouselComponents",
+  data() {
+    return {
+      // Liste des images locales importées
+      images: [
+        Joconde,
+        LeCri,
+        Monet,
+        Picasso,
+        Tournessol
+      ],
+      cycle: true,
+      height: "70vh",
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
